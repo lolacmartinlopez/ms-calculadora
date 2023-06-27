01dde9c32ebfc985c28382b1d57c99b549c88a9b
lolacmartinlopez <lola.cristina.martin.lopez@edag-ps.com>
2023-06-27 22:34:03 +0200

    Service operacion implemented

--- src/main/java/data/Operacion.java
+++ src/main/java/data/Operacion.java
@@ -1,7 +1,5 @@
 package data;
 
-import com.msbattleshipaccounts.msbattleshipaccounts.data.Player;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/repository/OperacionRepository.java
+++ src/main/java/repository/OperacionRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
-public class OperacionRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import data.Operacion;
+
+
+
+public interface OperacionRepository extends JpaRepository <Operacion, Long>{
+	List<Operacion> findById(String operacionId);
 }
